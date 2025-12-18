84995ef6370c85ce8208d7b549100b5b60b43c0e
YubI996 <sayidyubi28@gmail.com>
2025-12-18 11:43:20 +0800

    add Kelurahan resource with controller, requests, model, policy, factory, migration, and seeder

---
+++ app/Http/Controllers/KelurahanController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\kelurahan;
+use App\Http\Requests\StorekelurahanRequest;
+use App\Http\Requests\UpdatekelurahanRequest;
+
+class KelurahanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorekelurahanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(kelurahan $kelurahan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(kelurahan $kelurahan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatekelurahanRequest $request, kelurahan $kelurahan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(kelurahan $kelurahan)
+    {
+        //
+    }
+}
---
+++ app/Models/kelurahan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class kelurahan extends Model
+{
+    /** @use HasFactory<\Database\Factories\KelurahanFactory> */
+    use HasFactory;
+}
