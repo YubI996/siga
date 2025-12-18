3a23c382ff1813306061da4a39ecd605c9aa03ac
YubI996 <sayidyubi28@gmail.com>
2025-12-18 11:43:34 +0800

    add RT resource with controller, requests, model, policy, factory, migration, and seeder

---
+++ app/Http/Controllers/RtController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rt;
+use App\Http\Requests\StoreRtRequest;
+use App\Http\Requests\UpdateRtRequest;
+
+class RtController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRtRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Rt $rt)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Rt $rt)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRtRequest $request, Rt $rt)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Rt $rt)
+    {
+        //
+    }
+}
