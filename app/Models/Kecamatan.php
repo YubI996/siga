43f59742d6b47f0dbc44f813dd7166a775e9091c
YubI996 <sayidyubi28@gmail.com>
2025-12-18 11:04:06 +0800

    add HasUuids trait to Kecamatan model for UUID support

--- app/Models/Kecamatan.php
+++ app/Models/Kecamatan.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Kecamatan extends Model
 {
     /** @use HasFactory<\Database\Factories\KecamatanFactory> */
     use HasFactory;
+    use HasUuids;
+    
+    protected $fillable = [
+        'kode',
+        'nama',
+    ];
+
+    public function kelurahans()
+    {
+        return $this->hasMany(Kelurahan::class);
+    }
 }
