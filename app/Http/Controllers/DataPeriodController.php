b7be688abad2022a5a4e6cb6ba0f1a1b9b737f4d
YubI996 <sayidyubi28@gmail.com>
2025-12-18 11:45:30 +0800

    add DataPeriod resource with controller, requests, model, policy, factory, migration, and seeder

---
+++ app/Http/Controllers/DataPeriodController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DataPeriod;
+use App\Http\Requests\StoreDataPeriodRequest;
+use App\Http\Requests\UpdateDataPeriodRequest;
+
+class DataPeriodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDataPeriodRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DataPeriod $dataPeriod)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DataPeriod $dataPeriod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDataPeriodRequest $request, DataPeriod $dataPeriod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DataPeriod $dataPeriod)
+    {
+        //
+    }
+}
---
+++ database/seeders/DataPeriodSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DataPeriodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
