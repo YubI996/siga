7ffdb8ab2a26f4124f43417b38f43103663bf166
YubI996 <sayidyubi28@gmail.com>
2025-12-18 11:44:51 +0800

    add DataBatch resource with controller, requests, model, policy, factory, migration, and seeder

---
+++ app/Http/Controllers/DataBatchController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DataBatch;
+use App\Http\Requests\StoreDataBatchRequest;
+use App\Http\Requests\UpdateDataBatchRequest;
+
+class DataBatchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDataBatchRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DataBatch $dataBatch)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DataBatch $dataBatch)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDataBatchRequest $request, DataBatch $dataBatch)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DataBatch $dataBatch)
+    {
+        //
+    }
+}
