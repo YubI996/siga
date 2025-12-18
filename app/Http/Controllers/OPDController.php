fc65d98dc26f9c15da48af9fe5f1bbb0ad4adb4b
YubI996 <sayidyubi28@gmail.com>
2025-12-18 11:46:04 +0800

    add OPD resource with controller, requests, model, policy, factory, migration, and seeder

---
+++ app/Http/Controllers/OPDController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OPD;
+use App\Http\Requests\StoreOPDRequest;
+use App\Http\Requests\UpdateOPDRequest;
+
+class OPDController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOPDRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OPD $oPD)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OPD $oPD)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOPDRequest $request, OPD $oPD)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OPD $oPD)
+    {
+        //
+    }
+}
